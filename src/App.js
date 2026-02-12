af61290a074450e01854590d71c9ecabe2f7a10d
biggerlulalula <2362549204@qq.com>
2026-02-13 01:34:26 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// 项目的根组件
+// app -> index.js -> public/index.html(root) 
+const count =100
+const list=[
+{
+  id:1,
+  name:'tom'
+}
+,
+{
+  id:2,
+  name:'tom1'
+},
+{
+  id:3,
+  name:'tom1'
+}
+]
 
+function  message(){
+  return 'this is message'
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 渲染列表 */}
+      {/* 循环哪个结构就返回哪个结构 */}
+      {/* 加上一个独一无二的key */}
+      {/* key的作用：react框架的内部使用 提升更新性能的 */}
+      <ul>
+        {
+          list.map(item=>
+            <li key={item.id}>{item.name}</li>)
+        }
+        </  ul  >
+      this is App
+       {/*  引号传递字符串*/}
+      {'this is message'}
+      {/* 识别js变量 */}
+      {count}
+      {/* 函数调用 */}
+      {message()}
+      {/*方法调用 */}
+       {new Date().getTime()}
+       {/* 使用js对象 */}
+       <div style={{color:'red'}}>
+         this is div
+       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
